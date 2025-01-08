07d09172d742381b293d9d8dfce60e266d186dcb
Mateus707 <darkp331@gmail.com>
2025-01-07 21:05:18 -0300

    Logradouro Model - GET´S E SET´S completos

---
+++ EDUCACAO2/model/logradoroModel.php
@@ -0,0 +1,65 @@
+<?php 
+    class LogradouroModel {
+        private $idLogradouro,$ruaLogrado,$numeroLogrado,$bairroLogrado,$cidadeLogrado,$estadoLogrado,$cepLogrado,$complementoLogrado;
+
+
+        public function getIdLogradouro(){
+         return $this->idLogradouro;
+        }
+        public function setIdLogradouro($idLogradouro){
+            $this->idLogradouro = $idLogradouro;
+        }
+        public function getRuaLogrado(){
+         return $this->ruaLogrado;
+        }
+        public function setRuaLogrado($ruaLogrado){
+            $this->ruaLogrado = $ruaLogrado;
+        }
+        public function getNumeroLogrado(){
+            return $this->numeroLogrado;
+        }
+        public function setNumeroLogrado($numeroLogrado){
+            $this->numeroLogrado = $numeroLogrado;
+        }
+        public function getBairroLogrado(){
+            return $this->bairroLogrado;
+        }
+        public function setBairroLogrado($bairroLogrado){
+            $this->bairroLogrado = $bairroLogrado;
+        }
+        public function getCidadeLogrado() {
+            return $this->cidadeLogrado;
+        }
+        
+        public function setCidadeLogrado($cidadeLogrado) {
+            $this->cidadeLogrado = $cidadeLogrado;
+        }
+        
+        public function getEstadoLogrado() {
+            return $this->estadoLogrado;
+        }
+        
+        public function setEstadoLogrado($estadoLogrado) {
+            $this->estadoLogrado = $estadoLogrado;
+        }
+        
+        public function getCepLogrado() {
+            return $this->cepLogrado;
+        }
+        
+        public function setCepLogrado($cepLogrado) {
+            $this->cepLogrado = $cepLogrado;
+        }
+        
+        public function getComplementoLogrado() {
+            return $this->complementoLogrado;
+        }
+        
+        public function setComplementoLogrado($complementoLogrado) {
+            $this->complementoLogrado = $complementoLogrado;
+        }
+        
+        
+    }
+
+?>
