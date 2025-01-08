e4a1b7d7b738ebe19c61a25265d54560d813f38f
Mateus707 <darkp331@gmail.com>
2025-01-07 21:03:51 -0300

    Usuario Dao - Cruds completos

--- EDUCACAO2/DAO/usuarioDAO.php
+++ EDUCACAO2/DAO/usuarioDAO.php
@@ -1,27 +1,42 @@
 <?php
     require_once(__DIR__.'/../model/conexao.php');
-    require_once(__DIR__.'/../model/testconexao.php');
 
     class UsuarioDAO{
+        private $conexao;
+        
+            public function __construct() {
+                $this->conexao = Conexao::conexaoBanco_de_Dados();
+                if (!$this->conexao) {
+                    throw new Exception("Erro ao conectar ao banco de dados.");
+                }
+            }
+        
+        
 
-        public static function createUser($user){
+
+        public static function createUser($usuario){
             //realiza a conexão com banco de dados 
             $conexao = Conexao::conexaoBanco_de_Dados();
             //QUErty do banco de dados sendo preparads para ser executado no banco
             $query = "INSERT INTO tbUsuario(nomeUsuario,emailUsuario,senhaUsuario) values(?,?,?)";
-            $stmt = $conexao->prepare(query: $query);
+            $stmt = $conexao->prepare($query);
             //valores sendo vinculados aos parametros nas cosnsultas 
-            $stmt ->bindValue(1,$user->getNomeUsuario());
-            $stmt ->bindValue(2,$user->getEmailUsuario());
-            $stmt ->bindValue(3,$user->getSenhaUsuario());
-            return $stmt -> execute();
+            $stmt ->bindValue(1,$usuario->getNomeUsuario());
+            $stmt ->bindValue(2,$usuario->getEmailUsuario());
+            $stmt ->bindValue(3,$usuario->getSenhaUsuario());
+            if ($stmt->execute()) {
+                return $conexao->lastInsertId();
+            }else{
+                return throw new Exception("Erro ao inserir usuário: " . implode(", ", $stmt->errorInfo()));
+            }
+             
         }
 
         public static function showById($id){
             //realiza a conexão com banco de dados 
             $conexao = Conexao::conexaoBanco_de_Dados();
             //QUErty do banco de dados sendo preparads para ser executado no banco
-            $query = "SELECT * FROM tbUsuario WHERE id = ? ";
+            $query = "SELECT * FROM tbUsuario WHERE idUsuario = ? ";
             $stmt = $conexao->prepare(query: $query);
             //valores sendo vinculados aos parametros nas cosnsultas 
             $stmt ->bindValue(1,$id);
@@ -39,7 +54,7 @@
             return $stmt->fetchAll();
         }
 
-        public static function putUser($id,$user){
+        public static function putUser($id,$usuario){
             //realiza a conexão com banco de dados 
             $conexao = Conexao::conexaoBanco_de_Dados();
             //QUErty do banco de dados sendo preparads para ser executado no banco
@@ -47,12 +62,12 @@
              nomeUsuario = ?,
              emailUsuario = ?,
              senhaUsuario = ?
-             WHERE id = ?";
+             WHERE idUsuario = ?";
             $stmt = $conexao->prepare(query: $query);
             //valores sendo vinculados aos parametros nas cosnsultas 
-            $stmt ->bindValue(1,$user->getNomeUsuario());
-            $stmt ->bindValue(2,$user->getEmailUsuario());
-            $stmt ->bindValue(3,$user->getSenhaUsuario());
+            $stmt ->bindValue(1,$usuario->getNomeUsuario());
+            $stmt ->bindValue(2,$usuario->getEmailUsuario());
+            $stmt ->bindValue(3,$usuario->getSenhaUsuario());
             $stmt ->bindValue(4 ,$id);
             return $stmt -> execute();
         }
@@ -68,12 +83,9 @@
                  return $stmt -> execute();
         }
 
-
-
-
         public static function checkCredentials($email, $senha){
             $conexao = Conexao::conexaoBanco_de_Dados();
-            $query = "SELECT * FROM user WHERE emailUser = ? and passwordUser = ?";
+            $query = "SELECT * FROM tbUsuario WHERE emailUsuario = ? and senhaUsuario = ? AND emailVerificadoUsuario=1";
             $stmt = $conexao->prepare($query);
             $stmt->bindValue(1, $email);
             $stmt->bindValue(2, $senha);
@@ -86,6 +98,30 @@
                 }   
           
           
+        }
+
+        public static function putCheckEmail($idUsuario){
+           try{
+            //realiza a conexão com banco de dados 
+            $conexao = Conexao::conexaoBanco_de_Dados();
+            //QUErty do banco de dados sendo preparads para ser executado no banco
+            $query = "UPDATE tbUsuario SET
+             emailVerificadoUsuario = ?
+             WHERE idUsuario = ?";
+            $stmt = $conexao->prepare($query);
+            //valores sendo vinculados aos parametros nas cosnsultas 
+            $stmt ->bindValue(1,1);
+            $stmt ->bindValue(2,$idUsuario);
+            if ($stmt->execute()) {
+                return true; // Sucesso ao atualizar o registro
+            } else {
+                // Lança uma exceção caso ocorra algum erro na execução
+                throw new Exception("Erro ao atualizar o usuário: " . implode(", ", $stmt->errorInfo()));
+            }
+        } catch (Exception $e) {
+            // Trata e relança a exceção para ser manipulada fora do método
+            throw new Exception("Erro no método putCheckEmail: " . $e->getMessage());
+        }
         }
 
     }
